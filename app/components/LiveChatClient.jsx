911de93a4d17569258a6410326029622cd400a13
ravifst1995-blip <ravifst1995@gmail.com>
2026-01-29 06:03:44 +0000

    newChat

--- app/components/LiveChatClient.jsx
+++ app/components/LiveChatClient.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { LiveChatWidget } from "@livechat/widget-react";
+import dynamic from "next/dynamic";
 
-
-
+const LiveChatWidget = dynamic(
+  () => import("@livechat/widget-react").then((mod) => mod.LiveChatWidget),
+  { ssr: false }
+);
 
 export default function LiveChatClient() {
-  return (
-   <LiveChatWidget license="19476814" />
-  );
+  return <LiveChatWidget license="19476814" />;
 }
